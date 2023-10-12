fed15f53c227075038fda71ecbe25dfb7564406d
Estefany Clara <estefany.clara@correo.ucu.edu.uy>
2023-10-11 21:21:41 -0300

    Actualizacion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { JugadorComponent } from './jugador/jugador.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
    
-    JugadorComponent
+    JugadorComponent,
+        HeaderComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/jugador/jugador.component.ts
+++ src/app/jugador/jugador.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
-import { JUGADOR } from '../lista-jugadores';
+import { Component, OnInit } from '@angular/core';
+import { Jugador } from '../jugador';
+import { JugadorService } from '../jugador.service';
+import { FormGroup, FormControl } from '@angular/forms';
+
 @Component({
   selector: 'app-jugador',
   templateUrl: './jugador.component.html',
   styleUrls: ['./jugador.component.css']
 })
-export class JugadorComponent {
-  jugadores= JUGADOR;
+export class JugadorComponent implements OnInit{
+  jugadores: Jugador[]=[];
+
+  constructor(private jugadorService:JugadorService ) { }
+
+  ngOnInit(): void {
+      this.jugadorService.getJugadores().subscribe(data=>{
+        this.jugadores = data;
+      });
+  }
+
+  
 }
